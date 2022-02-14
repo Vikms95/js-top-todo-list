c06ff99696f020eaa9eee602dc4e4691189be89a
Vikms95 <vms1995@hotmail.com>
2022-02-14 22:51:49 +0100

    Refactor delete from todo

--- notes.md
+++ notes.md
@@ -69,6 +69,8 @@ PROJECT GENERAL SCHEMA:
     ! Was not returning when the loop was finished
 <!-- - Do not let a todo be on more than one project: if todo projectTitleItBelongs is not '', quit -->
 <!-- - Call the function that removes array of todos from project and sets projectTitleItBelongs = '' -->
+
+- WORK ON REFACTORING FOR LOOPS DELETE ELEMENTS L11
 - Append to the projecs todo-list-container it's todo with the   reduced views (renderViewTodoFromProject)
 - Look into date library
 https://www.section.io/engineering-education/javascript-dates-manipulation-with-date-fns/
--- src/logic/deleteElements.js
+++ src/logic/deleteElements.js
@@ -3,28 +3,49 @@ import { todosStorage, projectsStorage } from './objectsStorage'
 // This function is called whenever a todo is shown on display
 function addEventListenerTodoDeleteButton(element, todoObject) {
     element.addEventListener('click', () => {
+        //TODO Refactor and use array method
+        // Find which project it belong to by checking projectTitleItBelongs, and stores the object info
+        const projectToRemoveTodo = projectsStorage
+            .find(project => project.title === todoObject.projectTitleItBelongs) 
+
+        //TODO ON WORK
+        //Remove todo from project's array
+        projectToRemoveTodo._attachedProjectTodos
+            .map(todo => {
+                if(todo._id === todoObject._id){
+                    todo = ''
+                }
+            })
+
+        console.log(projectToRemoveTodo._attachedProjectTodos)
+
+    
+        //TODO Find todo id inside todo's array
+
+
+        // let todoToDelete = projectItBelongs._attachedProjectTodos
+        //     .find(todo => todo._id === todoObject._id)
+        
+        // Delete todo's projectItBelongsProperty
+
+        
+
+        // for (let j = 0; j < projectsStorage._attachedProjectTodos.length; j++) {
+            
+        //     if(projectsStorage._attachedProjectTodos[j]._id === todoObject._id){
+                
+        //         // If found, make that index = ""
+        //         projectsStorage._attachedProjectTodos[j] = ''
+        //         todoObject.projectTitleItBelongs = ''
+        //     }
+        // }
+
         // Call the function that removes todo from display (deleting child element)
         element.parentNode.parentNode.remove()
     
         // Call the function that removes todo from general todo array
         deleteElementFromStorageArray(todoObject, todosStorage)
         
-        //TODO Refactor and use array method
-        // Find which project it belong to by checking projectTitleItBelongs
-        for (let i = 0; i < projectsStorage.length; i++) {
-            
-            if (projectsStorage[i].title === todoObject.projectTitleItBelongs) {
-                // Find todo id inside project's array
-                for (let j = 0; j < projectsStorage._attachedProjectTodos.length; j++) {
-                    if(projectsStorage._attachedProjectTodos[j]._id === todoObject._id){
-            
-                        // If found, make that index = ""
-                        projectsStorage._attachedProjectTodos[j] = ''
-                        todoObject.projectTitleItBelongs = ''
-                    }
-                }
-            }
-        }
     })
 }
 
@@ -40,9 +61,6 @@ function addEventListenerProjectDeleteButton(element, projectObject) {
 
         // Call the function that removes project from general project array
         deleteElementFromStorageArray(projectObject, projectsStorage)
-
-        
-
     })
 }
 
@@ -58,3 +76,4 @@ function deleteElementFromStorageArray(object, array) {
         }
     }
 }
+
